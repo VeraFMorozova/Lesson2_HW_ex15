d63552167a1e1a3a0d67a2aadcf949b537aa04c1
VeraMorozova <veraff@yandex.ru>
2022-12-25 20:54:46 +0500

    решение задачи 10

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: 
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int getNumberFromRange(int start, int end)
+{
+    int number = new Random().Next(start, end+1);
+    return number;
+}
+int SecondDigit(int number)
+{
+   int digit3 = number / 10;
+   int result = digit3 % 10;
+   return result;
+} 
+int randomNumber = getNumberFromRange(100,999);
+int numberSecond = SecondDigit(randomNumber);
+Console.WriteLine($"Вторая цифра числа {randomNumber} равна {numberSecond}");
